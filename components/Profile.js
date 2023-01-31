0f9ec24e2785c7591e834283a9a4c84ea253f41b
Aircenam Suniemi <winwininnovation@gmail.com>
2023-01-31 07:10:43 +0000

    done

--- components/Profile.js
+++ components/Profile.js
@@ -1,15 +1,14 @@
 import Image from "next/image";
-import profile from "../public/img04-profile.png";
 
 export default function Profile() {
   return (
     <div>
       <Image
         class="w-10 h-10 rounded-full cursor-pointer"
-        src={profile}
+        src="https://images.unsplash.com/photo-1555888997-03e986fc157b?ixlib=rb-4ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8b2xkJTIwbWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
         alt="profile"
-        width={100}
-        height={100}
+        width={500}
+        height={500}
       />
     </div>
   );
