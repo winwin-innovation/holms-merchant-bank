9a9a407c2992d3fbfb93c3cfb27bffe8280bf9e1
Aircenam Suniemi <winwininnovation@gmail.com>
2023-02-05 22:44:34 +0000

    done

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="icon" href="../../public/fav.png" />
+
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
